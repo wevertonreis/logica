7bcde78ea073bd1898225134ffd3e057c57b4d1d
Weverton Reis <146130724+wevertonreis@users.noreply.github.com>
2023-10-02 17:24:51 -0300

    if

---
+++ 01.js
@@ -0,0 +1,10 @@
+//if,else if ,else
+let nivelDeFome =  1
+
+if(nivelDeFome === 1){
+console.log("pouca fome")
+}else if(nivelDeFome ===2){
+console.log("muita fome")
+}else{
+console.log("Você comeria mais que o pica pau")
+}
--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-let nomeHero = "Wst";
-let pxHero = 1000;
+let nomeHero = "Wst"
+let pxHero = 6755
 
-switch (pxHero) {
-    case  0 <= 1000:
-        console.log(" ferro ");
-        
-    case 1001 <= 2000:
-        
-    case 2001 <= 3000:
-
-    case 3001 <= 4000:
-
-    case 4001 <= 5000:
-
-    case 5001 <= 6000:
-
-    case 6001 <= 7000:
-
-    case 7001 <= 8000:
-
-    case 8001 <= 9000:
-
-    case 9001 <= 10000:
-
-    case 10001 :
-}
-
-console.log("O Hero de nome ", nomeHero, " esta no nivel de ", pxHero);
+if (pxHero < 1001) {
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de ferro" )
+}else if (pxHero >= 1001 === pxHero < 2001){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de bronze" )
+}else if (pxHero >= 2001 === pxHero < 3000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de prata" )
+}else if (pxHero >= 2001 === pxHero < 3000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de ouro" );
+}else if (pxHero >= 3001 === pxHero < 4000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de platina")
+}else if (pxHero >= 4001 === pxHero < 5000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de ascendente")
+}else if (pxHero >=5001 === pxHero < 6000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de imortal" )
+}else if (pxHero >= 6001 === pxHero < 7000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de radiante" )
+}else if (pxHero >= 7001 === pxHero < 8000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de radiante 2")
+}else if (pxHero >= 8001 === pxHero < 9000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de radiante 3")
+}else if (pxHero >= 9001 === pxHero < 10000){
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de rubi" )
+}else 
+    console.log("O Hero de nome ", nomeHero, " esta no nivel de esmeralda")
