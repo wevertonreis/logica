fa8955a231af63972a03166a8c7d995006ddf9ce
Weverton Reis <146130724+wevertonreis@users.noreply.github.com>
2023-10-02 15:16:06 -0300

    desafio

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("ola mundo")
+let nomeHero = "Wst";
+let pxHero = 1000;
+
+switch (pxHero) {
+    case  0 <= 1000:
+        console.log(" ferro ");
+        
+    case 1001 <= 2000:
+        
+    case 2001 <= 3000:
+
+    case 3001 <= 4000:
+
+    case 4001 <= 5000:
+
+    case 5001 <= 6000:
+
+    case 6001 <= 7000:
+
+    case 7001 <= 8000:
+
+    case 8001 <= 9000:
+
+    case 9001 <= 10000:
+
+    case 10001 :
+}
+
+console.log("O Hero de nome ", nomeHero, " esta no nivel de ", pxHero);
