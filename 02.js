b30c48a93186df0d4cdcad74ba54c0761564573a
Weverton Reis <146130724+wevertonreis@users.noreply.github.com>
2023-10-16 07:16:07 -0300

    5

---
+++ 02.js
@@ -0,0 +1,9 @@
+
+
+function soma(vitorias, derrotas) {
+        return vitorias - derrotas;
+
+}
+
+let resultado = soma(55,10);
+console.log("O Herói tem de saldo de", soma, "está no nível de **{nivel}**")
